7903ae698ba4aa9045242777a0963c5bc9119e4d
Nathan Roberts <93275916+Mubioh@users.noreply.github.com>
2022-12-22 13:18:20 +0000

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,17 @@
 const { Events, MessageType, ChannelType } = require('discord.js');
 
+const commentEnabledChannelIds = ['1055439884388663306'];
+
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.type === MessageType.ChannelPinnedMessage) message.delete();
-		if (message.channel.id === '1055439884388663306') await message.startThread({ name: 'Comments' });
 		if (message.channel.type === ChannelType.GuildAnnouncement && message.crosspostable)
 			message.crosspost();
+		for (const id of commentEnabledChannelIds) {
+			if (message.channel.id === id) {
+				await message.startThread({ name: 'Comments' });
+			}
+		}
 	},
 };
